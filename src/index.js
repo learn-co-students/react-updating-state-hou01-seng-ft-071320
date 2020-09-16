3a2d9d4ccf4265202c2c9d8f6755d990dff07def
JuanToanXoup <Johndoan42@gmail.com>
2020-09-15 21:55:24 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick';
+ 
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
 
 ReactDOM.render(
   <div>
